09ac9fa14376e308d1e25663a7ea3ee8847a27ff
Mahmud Wunnam <38923023+MahmudSuleman@users.noreply.github.com>
2025-07-06 17:45:01 +0000

    create main menu in the home

--- CMS/Controllers/HomeController.cs
+++ CMS/Controllers/HomeController.cs
@@ -10,12 +10,93 @@ namespace CMS.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(GetMenuItems());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View();
+        }
+
+        private List<MenuItem> GetMenuItems()
+        {
+            return
+            [
+                new()
+                {
+                    Name = "Human Resources",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-users",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Programmes & Projects",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-project-diagram",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Tender",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-file-signature",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Contracts",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-handshake",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Payments",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-money-bill-wave",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Variation Order",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-exchange-alt",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Accounts & Audits",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-calculator",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Reports",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-chart-bar",
+                    Link = "#"
+                },
+                new()
+                {
+                    Name = "Administration",
+                    Count = 10,
+                    Description = "Assigned to you",
+                    Icon = "fas fa-cog",
+                    Link = Url.Action("Index", "Home", new { area = "Setups" }),
+                    HasCount = false
+                }
+            ];
         }
     }
 }
---
+++ CMS/MenuItems.cs
@@ -0,0 +1,12 @@
+﻿namespace CMS
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; } // Can use Font Awesome, Bootstrap Icons, etc.
+        public string Link { get; set; }
+        public int Count { get; set; }
+        public bool HasCount { get; set; } = true; 
+    }
+}
