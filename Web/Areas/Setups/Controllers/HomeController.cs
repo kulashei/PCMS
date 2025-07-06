23e2b4a52b9d83e8d175d793a6c4d794e82384b9
Mahmud Wunnam <38923023+MahmudSuleman@users.noreply.github.com>
2025-07-06 06:18:26 +0000

    initial commit

---
+++ Web/Areas/Setups/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Setups.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Breadcrumb = "Setups Dashboard";
+            return View();
+        }
+    }
+}
